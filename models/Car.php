c5f5b8ccb40911ee077c9d5ef5effe41096fcca0
walterdavis <waltd@wdstudio.com>
2013-03-19 22:36:11 -0400

    fixed the test

--- models/Car.php
+++ models/Car.php
@@ -3,6 +3,7 @@ class Car extends MiniActiveRecord {
   public $validations = 'presence:model; regexp:year:/\d{4}/; presence:year';
   public $has_many = 'accidents';
   public $has_many_through = 'dents:accidents';
+  public $has_and_belongs_to_many = 'drivers';
   function description(){
     return implode(' ', array($this->year, $this->color, $this->model));
   }
